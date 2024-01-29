b6e45d9a509b960ce06e133f37f69613dcb62f10
Orcun Yildiz <oyildiz@beboplogin3.lcrc.anl.gov>
2024-01-29 15:48:13 -0600

    updating the sample actions.py

--- examples/lowfive/actions/actions.py
+++ examples/lowfive/actions/actions.py
@@ -1,12 +1,12 @@
 dw_counter = 0
 
-def callback(vol):
+def callback(vol, rank):
     def adw_cb():
         global dw_counter
         import sys
         dw_counter = dw_counter + 1
         if dw_counter % 4 == 0:
             print("calling adw callback")
-            sys.stdout.flush()
-            vol.serve_all(False)
+            vol.serve_all(True, True)
     vol.set_after_dataset_write(adw_cb)
+    vol.serve_on_close = False
