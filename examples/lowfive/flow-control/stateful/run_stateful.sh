b0f19e654157c13000681ac95b34405c7d89c542
Orcun Yildiz <oyildiz@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2025-03-06 16:11:13 +0000

    updating config files in flow-control examples to reflect the changes in wilkins-master

--- examples/lowfive/flow-control/stateful/run_stateful.sh
+++ examples/lowfive/flow-control/stateful/run_stateful.sh
@@ -1,10 +1,10 @@
 if [[ $1 == '-single' ]]
 then
     echo "Generating same file over the course of simulation execution"
-    mpirun -n 2 -l python -u wilkins-master.py wilkins_prod_con_singleFile.yaml -s
+    mpirun -n 2 -l python -u wilkins-master.py wilkins_prod_con_singleFile.yaml
 else
     echo "Generating different files over the course of simulation execution"
-    mpirun -n 2 -l python -u wilkins-master.py wilkins_prod_con.yaml -s
+    mpirun -n 2 -l python -u wilkins-master.py wilkins_prod_con.yaml
 fi
 
 
